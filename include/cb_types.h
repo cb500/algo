16a35f700628b2ba592819ffcafd19e37969965b
Charly Batista <carlbsb@localhost>
2021-06-04 16:22:13 -0300

    Make use of <stdbool.h>

--- include/cb_types.h
+++ include/cb_types.h
@@ -6,8 +6,11 @@
 
 #ifndef __CB_TYPES_H
 #define __CB_TYPES_H
+#if defined __STDC__ && defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
+#include <stdbool.h>
+#endif
 
-typedef enum 
+typedef enum __attribute__ ((__packed__))
 {
     CB_OK            = 0,
     CB_ERR_MEM_INIT  = -1,
@@ -15,12 +18,17 @@ typedef enum
     CB_ERR_GENERIC   = -99
 } CB_RETURN;
 
-typedef enum
+#ifdef bool
+typedef bool CB_BOOL;
+#define CB_TRUE true
+#define CB_FALSE false
+#else
+typedef enum __attribute__ ((__packed__))
 {
     CB_TRUE          = 1,
     CB_FALSE         = 0
 } CB_BOOL;
-
+#endif
 
 
 #endif // __CB_TYPES_H
